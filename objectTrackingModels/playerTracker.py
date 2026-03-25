b978ceb8bcb36e4ba33bc9461425955e8cb7d62e
Jessica Pfahler <jgpfahler@gmail.com>
2026-03-25 11:04:17 -0400

    adjusted window viewing dimensions and coloring

--- objectTrackingModels/playerTracker.py
+++ objectTrackingModels/playerTracker.py
@@ -8,34 +8,27 @@ model = YOLO("objectTrackingModels/runs/detect/train2/weights/best.pt")
 sct = mss.mss()
 
 monitor = {
-    "top": 100,
-    "left": 100,
-    "width": 800,
-    "height": 600
+    "top": 200,
+    "left": 0,
+    "width": 600,
+    "height": 400
 }
 
-# monitor = sct.monitors[1]
 
 while True:
     screenshot = sct.grab(monitor)
 
     frame = np.array(screenshot)
 
-    # If the frame has alpha channel (4 channels), drop it and convert to RGB
-    if frame.shape[2] == 4:
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2RGB)
-    # If the frame is already 3 channels (BGR), just convert to RGB
-    elif frame.shape[2] == 3:
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    
-    
-    # frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2RGB)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2RGB)
 
-    results = model(frame, conf=0.3)
+    results = model(frame, conf=0.1)
 
     annotated_frame = results[0].plot()
 
     cv2.imshow("YOLO Screen Tracking", annotated_frame)
+
+    cv2.moveWindow("YOLO Screen Tracking", 900, 0)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
